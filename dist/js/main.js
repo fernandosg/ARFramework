c9407ba4c05eb49ad3338deed5bdde167c6134e8
fsgom <fernandosegom@gmail.com>
2017-04-16 16:30:00 -0500

    Stop tracking files.

--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-//var Memorama=require("./memorama.js");
-//var ARFramework=require("./ar_framekwork.js");
-framework=new ARFramework({WIDTH:640,HEIGHT:480,canvas_id:"ra"});
-framework.init();
-var nivel=new Memorama();
-framework.addStage(nivel);
-nivel.calibracion();
-framework.start();//nivel.start();
