2f19537236ba3b9bad14f33a8b90fae1d80f0daa
fernandosg <fsg_tmp@hotmail.com>
2017-04-18 16:07:19 -0500

    Added a new method for handle the attachment process of a marker.

--- src/utils/detectormarker.js
+++ src/utils/detectormarker.js
@@ -2,10 +2,15 @@ function DetectorMarker(id,callback,puntero){
 	this.id=id;
 	this.callback=callback;
 	this.puntero=puntero;
+	this.attached=[];
 }
 
 DetectorMarker.prototype.detected = function() {
 	return this.callback;
 };
+
+DetectorMarker.prototype.attach=function(marker){
+	this.attached.push(new DetectorMarker(marker.id,marker.callback,marker.puntero));
+}
 
 module.exports=DetectorMarker;
