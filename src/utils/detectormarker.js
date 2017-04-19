594bbd0775d3d0cb66ed2c14d3ff7bad7ec6882d
fernandosg <fsg_tmp@hotmail.com>
2017-04-19 01:55:30 -0500

    Added in an internal stack of an specific marker when more that one marker is attached.

--- src/utils/detectormarker.js
+++ src/utils/detectormarker.js
@@ -3,6 +3,7 @@ function DetectorMarker(id,callback,puntero){
 	this.callback=callback;
 	this.puntero=puntero;
 	this.attached=[];
+	this.attached_id=[]
 }
 
 DetectorMarker.prototype.detected = function() {
@@ -10,7 +11,15 @@ DetectorMarker.prototype.detected = function() {
 };
 
 DetectorMarker.prototype.attach=function(marker){
+	this.attached_id.push(marker.id);
 	this.attached.push(new DetectorMarker(marker.id,marker.callback,marker.puntero));
 }
 
+DetectorMarker.prototype.hasAttachments=function(){
+	return this.attached.length>0;
+}
+
+DetectorMarker.prototype.getAttachmentsId=function(){
+	return this.attached_id;
+}
 module.exports=DetectorMarker;
